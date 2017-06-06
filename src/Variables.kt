fa1123d725ac18a71a05a87f90dc2e61653550a8
castillocode <jjandroid2015@gmail.com>
2017-06-06 16:42:56 -0400

    Creacion de variables diferencia entre var y val

---
+++ src/Variables.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by juanjosecastillo on 6/6/17.
+ */
+
+fun main (args: Array<String>)
+{
+    // val es como finally asignandole solo una vez
+
+    val nombre = "Juan Jose" //kotlin asume el tipo de dato
+
+    // el valor cuando es var puede cambiar en el proceso
+
+    var edad = 17
+
+    //explicitamente se puede colocar el tipo de variable
+
+    val nombre2 : String = "Juan jose"
+    var edad2 : Int = 21
+    var peso = 23
+
+    println("$nombre "+" $edad2")
+
+
+}
